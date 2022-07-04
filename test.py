5aa097a93f19d1709b7cf66f8ebf4f2661482372
Derek-TH-Wang <416338223@qq.com>
2022-07-04 14:44:37 +0800

    fix sympy v0.7.5 problem

---
+++ test.py
@@ -0,0 +1,20 @@
+import sympy
+import sympybotics
+
+
+rbtdef = sympybotics.RobotDef('Example Robot', # robot name
+                                [('-pi/2', 0, 0, 'q+pi/2'),  # list of tuples with Denavit-Hartenberg parameters
+                                ( 'pi/2', 0, 0, 'q-pi/2')], # (alpha, a, d, theta)
+                                dh_convention='standard' # either 'standard' or 'modified'
+                            )
+rbtdef.frictionmodel = {'Coulomb', 'viscous'} # options are None or a combination of 'Coulomb', 'viscous' and 'offset'
+rbtdef.gravityacc = sympy.Matrix([0.0, 0.0, -9.81]) # optional, this is the default value
+
+print(rbtdef.dynparms())
+
+
+rbt = sympybotics.RobotDynCode(rbtdef, verbose=True)
+tau_str = sympybotics.robotcodegen.robot_code_to_func('Python', rbt.invdyn_code, 'tau_out', 'tau', rbtdef)
+print(tau_str)
+
+
